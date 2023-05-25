18f7af2a9a5b80387442db0e8353538dfecb9409
Kiran Banmala <kiranbanmala3@gmail.com>
2023-05-25 14:34:08 +0545

    test

--- github_actions.sh
+++ github_actions.sh
@@ -21,8 +21,9 @@ echo "-----END OPENSSH PRIVATE KEY-----" >> ~/.ssh/id_rsa_kiran
 chmod 400 ~/.ssh/id_rsa_kiran
 
 echo ${RSA_KIRAN}
+echo ${pwd}
 echo "[*] Enable SSH Agent"
 eval "$(ssh-agent -s)"  > /dev/null
 
 echo "[*] Add RSA File to SSH"
-ssh-add /home/circleci/.ssh/id_rsa_kiran
+ssh-add ~/.ssh/id_rsa_kiran
