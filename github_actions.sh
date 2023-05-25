3f6d8107ce7d95c3e299adaf0f23de5358dae63c
Kiran Banmala <kiranbanmala3@gmail.com>
2023-05-25 14:48:27 +0545

    test

--- github_actions.sh
+++ github_actions.sh
@@ -4,24 +4,25 @@ mkdir -p ~/.ssh
 
 echo "[*] Create '~/.ssh/config' SSH config file"
 
-echo $(cat <<-EOF
+echo $(
+  cat <<-EOF
 Host github.com\n
-  HostName github.com
-  IdentityFile ~/.ssh/id_rsa_kiran
-  IdentitiesOnly yes
+  HostName github.com\n
+  IdentityFile ~/.ssh/id_rsa_kiran\n
+  IdentitiesOnly yes\n
 EOF
-) > ~/.ssh/config
+) >~/.ssh/config
 
 echo "[*] Create '~/.ssh/id_rsa_kiran' RSA file"
 
-echo "-----BEGIN OPENSSH PRIVATE KEY-----" > ~/.ssh/id_rsa_kiran
-echo ${RSA_KIRAN}  | sed 's/ /\n/g' >> ~/.ssh/id_rsa_kiran
-echo "-----END OPENSSH PRIVATE KEY-----" >> ~/.ssh/id_rsa_kiran
+echo "-----BEGIN OPENSSH PRIVATE KEY-----" >~/.ssh/id_rsa_kiran
+echo ${RSA_KIRAN} | sed 's/ /\n/g' >>~/.ssh/id_rsa_kiran
+echo "-----END OPENSSH PRIVATE KEY-----" >>~/.ssh/id_rsa_kiran
 
 chmod 400 ~/.ssh/id_rsa_kiran
 
 echo "[*] Enable SSH Agent"
-eval "$(ssh-agent -s)"  > /dev/null
+eval "$(ssh-agent -s)" >/dev/null
 
 echo "[*] Add RSA File to SSH"
 ssh-add ~/.ssh/id_rsa_kiran
